666ce486ddb3983a0adaecd890c542119aefa0a9
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-15 17:02:39 +0530

    updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-black  text-center mx-auto">
+      <h1 className="text-[3rem] font-bold font-mono ">Feed0</h1>
+      <p className="text-md">Forums for School / College</p>
+    </div>
+  );
+}
+
+export default App;
