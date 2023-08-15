3a6be526059c08918cbb2cb6104408d635b929e1
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-15 17:14:11 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="text-black  text-center mx-auto">
       <h1 className="text-[3rem] font-bold font-mono ">Feed0</h1>
-      <p className="text-md">Forums for School / College</p>
+      <p className="text-md">Forums for School / College / University</p>
     </div>
   );
 }
