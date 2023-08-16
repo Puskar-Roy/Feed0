36dcf6c0bf18e9b7846bfdd63fc312d17a007e71
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-16 21:03:38 +0530

    updated

--- frontend/src/Components/Home/HomeUI/SideNav.jsx
+++ frontend/src/Components/Home/HomeUI/SideNav.jsx
@@ -21,28 +21,28 @@ function SideNav() {
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaCompass className="mr-4 h-6 w-6" />
         My Feeds
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaBell className="mr-4 h-6 w-6" />
         Notifications
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaEnvelope className="mr-4 h-6 w-6" />
         Messages
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaBookmark className="mr-4 h-6 w-6" />
         Saved
@@ -50,14 +50,14 @@ function SideNav() {
 
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaUser className="mr-4 h-6 w-6" />
         Profile
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
       >
         <FaEllipsisH className="mr-4 h-6 w-6" />
         More
