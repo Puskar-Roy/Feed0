f68ec4cc62848137789b6c599f233b42a1e9e41f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-16 17:27:41 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import Channels from "./components/Channels";
-import Message from "./components/Message";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/channels">
-          <Channels />
-        </Route>
-        <Route path="/message">
-          <Message />
-        </Route>
         <Route path="/">
           <Home />
         </Route>
