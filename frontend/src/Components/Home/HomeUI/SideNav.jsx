209083d2fe386b6eaafb1d7a341cf3ae0e4dfd90
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-16 21:10:24 +0530

    updated

--- frontend/src/Components/Home/HomeUI/SideNav.jsx
+++ frontend/src/Components/Home/HomeUI/SideNav.jsx
@@ -11,38 +11,38 @@ import {
 
 function SideNav() {
   return (
-    <nav className="mt-5 px-2">
+    <nav className=" px-2 text-center text-white text-base font-semibold space-y-2">
       <a
         href="#"
-        className="group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full bg-blue-800 text-blue-300"
+        className=" flex items-center px-2 py-2      rounded-full bg-blue-800 "
       >
         <FaHome className="mr-4 h-6 w-6" />
         Home
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaCompass className="mr-4 h-6 w-6" />
         My Feeds
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaBell className="mr-4 h-6 w-6" />
         Notifications
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaEnvelope className="mr-4 h-6 w-6" />
         Messages
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaBookmark className="mr-4 h-6 w-6" />
         Saved
@@ -50,14 +50,14 @@ function SideNav() {
 
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaUser className="mr-4 h-6 w-6" />
         Profile
       </a>
       <a
         href="#"
-        className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300"
+        className="  flex items-center px-2 py-2       rounded-full  hover:bg-blue-300   text-black"
       >
         <FaEllipsisH className="mr-4 h-6 w-6" />
         More
