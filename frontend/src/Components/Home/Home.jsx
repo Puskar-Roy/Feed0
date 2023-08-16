90df9354ef14c33d65ad53742d81dc318734a792
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-16 19:46:32 +0530

    updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-
+import Posts from "./HomeUI/Posts/Posts";
 function Home() {
   return (
-    <div>
+    <div className="my-[8rem]">
       <div className="text-black  text-center mx-auto">
         <h1 className="text-[3rem] font-bold font-mono ">Feed0</h1>
 
         <p className="text-md">Forums for School / College / University</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 mx-auto">
+        <div>
+          <h1>sidenav</h1>
+        </div>
+        <div className="mx-auto">
+          <Posts />
+        </div>
+        <div>
+          <h1>sidenav</h1>
+        </div>
       </div>
     </div>
   );
