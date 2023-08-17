830eafd88ccaf9dc2d327e64e4687a45417a6d7e
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-17 16:48:47 +0530

    updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import Posts from "./HomeUI/Posts/Posts";
 import SideNav from "./HomeUI/SideNav";
 import CreatePost from "./HomeUI/Posts/CreatePost";
+import Navbar from "./HomeUI/NavBar";
 function Home() {
   return (
-    <div className="my-[8rem]">
-      <div className="text-black  text-center mx-auto">
-        <h1 className="text-[3rem] font-bold font-mono ">Feed0</h1>
+    <div className="mx-auto">
+      <Navbar />
+      <div className="my-[4rem]">
+        <div className="flex">
+          <div className="w-1/4 p-4">
+            <SideNav />
+          </div>
 
-        <p className="text-md">Forums for School / College / University</p>
-      </div>
-      <div className="flex">
-        <div className="w-1/4 p-4">
-          <SideNav />
-        </div>
+          {/* Main Content */}
+          <div className="w-1/2 p-4 space-y-10">
+            <CreatePost />
+            <Posts />
+          </div>
 
-        {/* Main Content */}
-        <div className="w-1/2 p-4 space-y-10">
-          <CreatePost />
-          <Posts />
-        </div>
-
-        <div className="w-1/4 p-4">
-          <h1>sidenav</h1>
+          <div className="w-1/4 p-4">
+            <h1>sidenav</h1>
+          </div>
         </div>
       </div>
     </div>
