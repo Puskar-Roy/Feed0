a964def1e4b7ea01c108a174e9e7d3de36276139
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-18 19:22:55 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
-
+import SearchResultsPage from "./Components/Home/HomeUI/Search/SearchResultsPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/searchresults" element={<SearchResultsPage />} />
       </Routes>
     </BrowserRouter>
   );
