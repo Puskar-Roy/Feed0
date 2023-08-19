e4642388dc26e1b1c71e1e55a3fa528d6a5e29c0
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 00:49:56 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
+++ frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
@@ -44,7 +44,7 @@ function TrendingUsers() {
   ];
 
   return (
-    <div className="overflow-y-auto space-y-4 w-full h-[18rem] mx-auto outline rounded-lg outline-gray-300  p-4 ">
+    <div className="overflow-y-auto space-y-4 w-full h-[18rem] mx-auto outline rounded-lg outline-gray-300  p-2 ">
       {suggestedPeople.map((person, index) => (
         <SuggestedPerson
           key={index}
