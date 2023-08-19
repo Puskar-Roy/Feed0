b58600c711f7eb1383bc381d5857c4686bbf8d71
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 03:24:09 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./Components/Auth/Register";
 import SearchResultsPage from "./Components/Home/HomeUI/Search/SearchResultsPage";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import NotificationsPage from "./Components/Home/HomeUI/Notifications/NotificationsPage";
+import Message from "./Components/Message/Message";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> */}
         <Route path="/profile" element={<UserProfile />} />
+        <Route path="/message" element={<Message />} />
         <Route path="/notifications" element={<NotificationsPage />} />
       </Routes>
     </BrowserRouter>
