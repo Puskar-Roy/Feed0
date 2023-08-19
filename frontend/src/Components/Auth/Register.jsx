6524e458cdf3487744cac299e761cc83b6524116
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-19 22:02:19 +0530

    updated

--- frontend/src/Components/Auth/Register.jsx
+++ frontend/src/Components/Auth/Register.jsx
@@ -67,7 +67,7 @@ const Register = () => {
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto ">
       <img className="w-8 h-8 mr-2" src="/Doraemon.png" alt="logo" />
 
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full bg-white rounded-xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Create an account
