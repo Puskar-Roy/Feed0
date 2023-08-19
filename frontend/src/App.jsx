043e28046f038c0bb4ad8d0be5cd9ff392bcb0c4
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 03:38:37 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,11 @@ import SearchResultsPage from "./Components/Home/HomeUI/Search/SearchResultsPage
 import UserProfile from "./Components/UserProfile/UserProfile";
 import NotificationsPage from "./Components/Home/HomeUI/Notifications/NotificationsPage";
 import Message from "./Components/Message/Message";
+import GlobalProvider from "./Components/Global/GlobalProvider";
 function App() {
   return (
     <BrowserRouter>
+      <GlobalProvider></GlobalProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/searchresults" element={<SearchResultsPage />} />
