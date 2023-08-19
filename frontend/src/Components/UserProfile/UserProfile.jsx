7481f5f5210b983ebf8e15fc0f43b00442cd40f4
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 03:54:52 +0530

    updated

--- frontend/src/Components/UserProfile/UserProfile.jsx
+++ frontend/src/Components/UserProfile/UserProfile.jsx
@@ -62,19 +62,15 @@ function UserProfile() {
               Solution Manager - Creative Tim Officer
             </p>{" "}
             <p className="mt-2 text-gray-500">University of Computer Science</p>{" "}
-          </div>{" "}
-          <div className="mt-12 flex flex-col justify-center">
-            {" "}
             <p className="text-gray-600 text-center font-light lg:px-16">
               An artist of considerable range, Ryan — the name taken by
               Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
               and records all of his own music, giving it a warm, intimate feel
               with a solid groove structure. An artist of considerable range.
-            </p>{" "}
-            <button className="text-indigo-500 py-2 px-4  font-medium mt-4">
-              {" "}
-              Show more
-            </button>{" "}
+            </p>
+          </div>{" "}
+          <div>
+            {/*  users posts here, ebr ekhane user r posts thabkba.. */}
           </div>
         </div>
       </div>
