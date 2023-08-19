7d1ea48e757c26b7c66cf34a009f79f4fc1653c1
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-19 21:03:18 +0530

    updated

--- frontend/src/Components/Auth/Login.jsx
+++ frontend/src/Components/Auth/Login.jsx
@@ -108,11 +108,6 @@ const Login = () => {
                   Remember me
                 </label>
               </div>
-              <p class="text-sm">
-                <a href="/forgot" class="text-indigo-400 hover:text-blue-500">
-                  Forgot password?
-                </a>
-              </p>
             </div>
             <div className="mt-4">
               <p className="text-center pb-3">Or continue with</p>
