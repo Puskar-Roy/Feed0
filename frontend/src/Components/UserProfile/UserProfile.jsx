68effd2201c487d2d6f9176a57816a661c8c14b5
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 03:58:44 +0530

    updated

--- frontend/src/Components/UserProfile/UserProfile.jsx
+++ frontend/src/Components/UserProfile/UserProfile.jsx
@@ -3,10 +3,8 @@ function UserProfile() {
     <div>
       <div className="p-16">
         <div className="p-8 bg-white shadow mt-24">
-          {" "}
           <div className="grid grid-cols-1 ">
             <div className="relative">
-              {" "}
               <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -14,65 +12,56 @@ function UserProfile() {
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
-                  {" "}
                   <path
                     fillRule="evenodd"
                     d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                     clipRule="evenodd"
                   />
-                </svg>{" "}
-              </div>{" "}
+                </svg>
+              </div>
               <button className="text-white py-2 px-4  rounded-full bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                {" "}
                 Edit Profile
-              </button>{" "}
-            </div>{" "}
+              </button>
+            </div>
             <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-              {" "}
               <div>
-                {" "}
-                <p className="font-bold text-gray-700 text-xl">22</p>{" "}
-                <p className="text-gray-400">Following</p>{" "}
-              </div>{" "}
+                <p className="font-bold text-gray-700 text-xl">22</p>
+                <p className="text-gray-400">Following</p>
+              </div>
               <div>
-                {" "}
-                <p className="font-bold text-gray-700 text-xl">10</p>{" "}
-                <p className="text-gray-400">Followers</p>{" "}
-              </div>{" "}
+                <p className="font-bold text-gray-700 text-xl">10</p>
+                <p className="text-gray-400">Followers</p>
+              </div>
               <div>
-                {" "}
-                <p className="font-bold text-gray-700 text-xl">89</p>{" "}
-                <p className="text-gray-400">Posts</p>{" "}
-              </div>{" "}
-            </div>{" "}
+                <p className="font-bold text-gray-700 text-xl">89</p>
+                <p className="text-gray-400">Posts</p>
+              </div>
+            </div>
             <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
               <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                {" "}
                 Follow
-              </button>{" "}
+              </button>
               <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                {" "}
                 Message
-              </button>{" "}
-            </div>{" "}
-          </div>{" "}
+              </button>
+            </div>
+          </div>
           <div className="mt-20 text-center border-b pb-12">
-            {" "}
             <h1 className="text-4xl font-medium text-gray-700">
-              Jessica Jones,{" "}
-            </h1>{" "}
-            <p className="font-light text-gray-600 mt-3">Bucharest, Romania</p>{" "}
+              Jessica Jones,
+            </h1>
+            <p className="font-light text-gray-600 mt-3">Bucharest, Romania</p>
             <p className="mt-8 text-gray-500">
               Solution Manager - Creative Tim Officer
-            </p>{" "}
-            <p className="mt-2 text-gray-500">University of Computer Science</p>{" "}
+            </p>
+            <p className="mt-2 text-gray-500">University of Computer Science</p>
             <p className="text-gray-600 text-center font-light lg:px-16">
               An artist of considerable range, Ryan — the name taken by
               Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
               and records all of his own music, giving it a warm, intimate feel
               with a solid groove structure. An artist of considerable range.
             </p>
-          </div>{" "}
+          </div>
           <div>
             {/*  users posts here, ebr ekhane user r posts thabkba.. */}
           </div>
