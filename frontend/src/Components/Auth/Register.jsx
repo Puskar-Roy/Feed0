598974415453248b334ae7634ec4e0b5ea75f62d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-19 21:27:31 +0530

    updated

--- frontend/src/Components/Auth/Register.jsx
+++ frontend/src/Components/Auth/Register.jsx
@@ -171,23 +171,7 @@ const Register = () => {
                   required
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="confirmPassword"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Upload Image
-                </label>
-                <input
-                  type="file"
-                  name="image"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  id="image"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
-              </div>
+
               <div className="flex items-start">
                 <div className="flex items-center h-5">
                   <input
