587f455e544e93392aaf55532eb1202bce8bb8df
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 00:56:49 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
+++ frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
@@ -40,11 +40,11 @@ function SuggestedPerson({ name, username, followers }) {
 function TrendingUsers() {
   const suggestedPeople = [
     { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
+    { name: "Feed0 Official", username: "Feed0", followers: 12000 },
+    { name: "Feed0 Official", username: "Feed0", followers: 120000000 },
+    { name: "Feed0 Official", username: "Feed0", followers: 100 },
+    { name: "Feed0 Official", username: "Feed0", followers: 1200 },
+
     // ... Add more suggested people
   ];
 
