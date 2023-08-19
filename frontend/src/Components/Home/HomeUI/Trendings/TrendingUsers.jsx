c0e893624464158759d34d2535c2e8c9a8fc6f5a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 00:36:35 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
+++ frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
@@ -38,8 +38,8 @@ function SuggestedPerson({ name, username, followers }) {
       </div>
       <div className="flex-1 px-4 py-2 m-2">
         <a href="">
-          <button className="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded-full">
-            <FaUserPlus className="mr-2 h-4 w-4 inline-block" /> Follow
+          <button className="bg-transparent hover:bg-blue-500  font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded-full">
+            <FaUserPlus className="h-4 w-4 " />
           </button>
         </a>
       </div>
