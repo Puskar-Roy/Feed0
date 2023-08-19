2bab2a9ead151f986e159bfd8d0bc5e29375bd7a
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-19 21:44:07 +0530

    updated

--- frontend/src/Components/Auth/Register.jsx
+++ frontend/src/Components/Auth/Register.jsx
@@ -213,7 +213,12 @@ const Register = () => {
               </div>
               <button
                 type="submit"
-                className="w-full text-blue bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className={`w-full text-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center ${
+                  termsAccepted
+                    ? "bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300"
+                    : "bg-blue-300 pointer-events-none"
+                } dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
+                disabled={!termsAccepted}
               >
                 Create an account
               </button>
