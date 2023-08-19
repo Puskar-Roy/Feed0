8a1aca5a448abb729b3793848c87281029090e71
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 01:07:26 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
+++ frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
@@ -43,7 +43,7 @@ function TrendingUsers() {
     { name: "ImKKingshuk", username: "Feed0", followers: 1200000000 },
     { name: "Puskar", username: "Feed0", followers: 120000000 },
     { name: "UIT", username: "Feed0", followers: 100 },
-    { name: "React", username: "Feed0", followers: 1200 },
+    { name: "", username: "Feed0", followers: 1200 },
 
     // ... Add more suggested people
   ];
