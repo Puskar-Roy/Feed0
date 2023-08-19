fea12efd92fbdd06235f83451efdcb8b62e887bd
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 01:07:49 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
+++ frontend/src/Components/Home/HomeUI/Trendings/TrendingUsers.jsx
@@ -40,7 +40,7 @@ function SuggestedPerson({ name, username, followers }) {
 function TrendingUsers() {
   const suggestedPeople = [
     { name: "Feed0 Official", username: "Feed0", followers: 1200000 },
-    { name: ", username: "Feed0", followers: 1200000000 },
+    { name: "ImKKingshuk", username: "Feed0", followers: 1200000000 },
     { name: "Puskar", username: "Feed0", followers: 120000000 },
     { name: "UIT", username: "Feed0", followers: 100 },
     { name: "Feed0 Official", username: "Feed0", followers: 1200 },
