cff70ac810eecf06f5f75aba98552bf01630f79f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-20 04:00:17 +0530

    updated

--- frontend/src/Components/UserProfile/UserProfile.jsx
+++ frontend/src/Components/UserProfile/UserProfile.jsx
@@ -24,10 +24,10 @@ function UserProfile() {
               </button>
             </div>
             <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-              <div>
+              <span>
                 <p className="font-bold text-gray-700 text-xl">22</p>
                 <p className="text-gray-400">Following</p>
-              </div>
+              </span>
               <div>
                 <p className="font-bold text-gray-700 text-xl">10</p>
                 <p className="text-gray-400">Followers</p>
